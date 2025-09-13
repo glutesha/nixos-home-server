8147cf7a76e43157d8f2f451b6f3bc2d7af2f4b7
glutesha <mail@glutesha.me>
2025-09-13 18:38:33 +0300

    Start flakes migration

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "My home server NixOS flake";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations = {
+        glutesha-server = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+          ];
+        };
+      };
+    };
+}
