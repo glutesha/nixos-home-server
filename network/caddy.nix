9262f568970e3efd718ed9ac59f1142ba28d2cb7
glutesha <mail@glutesha.me>
2025-09-24 20:07:10 +0300

    Set caddy host to local

--- network/caddy.nix
+++ network/caddy.nix
@@ -1,10 +1,12 @@
 {
   services.caddy = {
     enable = true;
-    virtualHosts."http://glutesha.local/jellyfin" = {
+
+    virtualHosts."http://127.0.0.1/jellyfin" = {
       extraConfig = ''
         reverse_proxy http://127.0.0.1:8096
       '';
     };
+
   };
 }
