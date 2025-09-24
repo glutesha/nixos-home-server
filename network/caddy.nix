7eb08313b3b468e3a63e0afbab3d24ab8d39a553
glutesha <mail@glutesha.me>
2025-09-24 20:04:34 +0300

    Disable auto HTTPS properly

--- network/caddy.nix
+++ network/caddy.nix
@@ -1,11 +1,10 @@
 {
   services.caddy = {
     enable = true;
-    virtualHosts."glutesha.local/jellyfin" = {
+    virtualHosts."http://glutesha.local/jellyfin" = {
       extraConfig = ''
         reverse_proxy http://127.0.0.1:8096
       '';
-      enableTLS = false;
     };
   };
 }
