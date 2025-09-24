56bfe9e1bf2da968099f508e17949a1647ac5e63
glutesha <mail@glutesha.me>
2025-09-24 20:24:07 +0300

    Experiment with handles #2

--- network/caddy.nix
+++ network/caddy.nix
@@ -4,7 +4,11 @@
 
     virtualHosts.":80" = {
       extraConfig = ''
-	handle_path /jellyfin* {
+	handle /jellyfin {
+	  redir /jellyfin/	
+	}
+
+	handle_path /jellyfin/ {
           reverse_proxy 127.0.0.1:8096
 	}
       '';
